8127255841ed2224240d2de7075ab3bb54573d19
Adam Scott <ascott.ca@gmail.com>
2022-09-11 18:42:55 -0400

    Add weak references to support preload cyclic references
    Add new algorithm for removing scripts based on dependencies
    Expose GDScriptCache::remove_script()
    Use .notest.gd system to bypass tests
    Add ScriptRef
    Add weakref for GDScriptParser
    Add global classes support (replaces PR#65664)
    Add GDScriptResource::get()
    Add packed scene script support

--- core/variant/variant_call.cpp
+++ core/variant/variant_call.cpp
@@ -1045,7 +1045,12 @@ void Variant::callp(const StringName &p_method, const Variant **p_args, int p_ar
 		}
 
 #endif
-		r_ret = _get_obj().obj->callp(p_method, p_args, p_argcount, r_error);
+		Ref<WeakRef> this_wref = *this;
+		if (this_wref.is_valid()) {
+			r_ret = this_wref->get_ref()._get_obj().obj->callp(p_method, p_args, p_argcount, r_error);
+		} else {
+			r_ret = _get_obj().obj->callp(p_method, p_args, p_argcount, r_error);
+		}
 
 	} else {
 		r_error.error = Callable::CallError::CALL_OK;
