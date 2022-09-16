091227b538b071edc380417f1d1dc98f63147517
Adam Scott <ascott.ca@gmail.com>
2022-09-15 23:09:45 -0400

    Add reference count check

--- modules/gdscript/gdscript_cache.cpp
+++ modules/gdscript/gdscript_cache.cpp
@@ -203,9 +203,15 @@ Ref<GDScript> GDScriptCache::get_shallow_script(const String &p_path, const Stri
 		singleton->dependencies[p_owner].insert(p_path);
 	}
 	if (singleton->full_gdscript_cache.has(p_path)) {
+		if (singleton->full_gdscript_cache[p_path]->reference_get_count() == 0) {
+			return Ref<GDScript>(nullptr);
+		}
 		return singleton->full_gdscript_cache[p_path];
 	}
 	if (singleton->shallow_gdscript_cache.has(p_path)) {
+		if (singleton->shallow_gdscript_cache[p_path]->reference_get_count() == 0) {
+			return Ref<GDScript>(nullptr);
+		}
 		return singleton->shallow_gdscript_cache[p_path];
 	}
 
