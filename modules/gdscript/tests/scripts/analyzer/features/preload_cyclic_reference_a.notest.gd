27a442978fff9ab077041026a858c9bff6819cfb
Adam Scott <ascott.ca@gmail.com>
2022-09-22 18:22:19 -0400

    Add easter egg in test

--- modules/gdscript/tests/scripts/analyzer/features/preload_cyclic_reference_a.notest.gd
+++ modules/gdscript/tests/scripts/analyzer/features/preload_cyclic_reference_a.notest.gd
@@ -1,6 +1,6 @@
 const B = preload("preload_cyclic_reference_b.notest.gd")
 
-const VALUE_TO_TEST = "godot"
+const WAITING_FOR = "godot"
 
 static func test_cyclic_reference():
 	B.test_cyclic_reference()
--- modules/gdscript/tests/scripts/analyzer/features/preload_cyclic_reference_b.notest.gd
+++ modules/gdscript/tests/scripts/analyzer/features/preload_cyclic_reference_b.notest.gd
@@ -1,4 +1,4 @@
 const A = preload("preload_cyclic_reference_a.notest.gd")
 
 static func test_cyclic_reference():
-	print(A.VALUE_TO_TEST)
+	print(A.WAITING_FOR)
