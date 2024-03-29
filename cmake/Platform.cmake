88134f125d04e6e4cbd97c4add54a982a2335871
Adam Scott <ascott.ca@gmail.com>
2024-03-29 11:48:04 -0400

    Add core and main build

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -1,14 +1,5 @@
 include_guard()
 
-set(BASE_SUPPORTED_PLATFORMS
-    "Android"
-    "LinuxBSD"
-    "MacOS"
-    "iOS"
-    "Web"
-    "Windows"
-)
-
 function(get_platform_name_from_system_name system_name platform_name)
     if(system_name STREQUAL "Android")
         set(platform_name "Android")
@@ -24,7 +15,18 @@ function(get_platform_name_from_system_name system_name platform_name)
         set(platform_name "MacOS")
     elseif(system_name STREQUAL "Windows")
         set(platform_name "Windows")
+	else()
+		set(platform_name "Unknown")
     endif()
 
     return(PROPAGATE platform_name)
+endfunction()
+
+function(get_platform_dir platform_name platform_dir)
+	string(TOLOWER ${platform_name} platform_dir_name)
+	if(IS_DIRECTORY ${Godot_SOURCE_DIR}/platform/${platform_dir_name})
+		set(platform_dir ${Godot_SOURCE_DIR}/platform/${platform_dir_name} PARENT_SCOPE)
+	else()
+		message(FATAL_ERROR "Platform not found: \"${platform_name}\"")
+	endif()
 endfunction()
