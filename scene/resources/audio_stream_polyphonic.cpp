72919cca6be5283b639c1b99537abae72e287ca6
Adam Scott <ascott.ca@gmail.com>
2024-06-08 11:34:40 -0400

    Rename shadowing variable

--- scene/resources/audio_stream_polyphonic.cpp
+++ scene/resources/audio_stream_polyphonic.cpp
@@ -228,18 +228,18 @@ AudioStreamPlaybackPolyphonic::ID AudioStreamPlaybackPolyphonic::play_stream(con
 					AudioServer::get_singleton()->register_stream_as_sample(p_stream);
 				}
 				float linear_volume = Math::db_to_linear(p_volume_db);
-				Ref<AudioSamplePlayback> sample_playback;
-				sample_playback.instantiate();
-				sample_playback->stream = streams[i].stream;
-				sample_playback->offset = p_from_offset;
-				sample_playback->volume_vector.resize(4);
-				sample_playback->volume_vector.write[0] = AudioFrame(linear_volume, linear_volume);
-				sample_playback->volume_vector.write[1] = AudioFrame(linear_volume, /* LFE= */ 1.0f);
-				sample_playback->volume_vector.write[2] = AudioFrame(linear_volume, linear_volume);
-				sample_playback->volume_vector.write[3] = AudioFrame(linear_volume, linear_volume);
-				sample_playback->bus = p_bus;
-				streams[i].stream_playback->set_sample_playback(sample_playback);
-				AudioServer::get_singleton()->start_sample_playback(sample_playback);
+				Ref<AudioSamplePlayback> sp;
+				sp.instantiate();
+				sp->stream = streams[i].stream;
+				sp->offset = p_from_offset;
+				sp->volume_vector.resize(4);
+				sp->volume_vector.write[0] = AudioFrame(linear_volume, linear_volume);
+				sp->volume_vector.write[1] = AudioFrame(linear_volume, /* LFE= */ 1.0f);
+				sp->volume_vector.write[2] = AudioFrame(linear_volume, linear_volume);
+				sp->volume_vector.write[3] = AudioFrame(linear_volume, linear_volume);
+				sp->bus = p_bus;
+				streams[i].stream_playback->set_sample_playback(sp);
+				AudioServer::get_singleton()->start_sample_playback(sp);
 			}
 
 			return (ID(i) << INDEX_SHIFT) | ID(streams[i].id);
