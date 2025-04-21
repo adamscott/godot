98a7f74830adfaeb37a5e8efcb152fd6bc4108ac
Adam Scott <ascott.ca@gmail.com>
2025-04-21 19:57:11 -0400

    Fix zstd decompression

--- platform/web/js/modules/zstd/index.mjs
+++ platform/web/js/modules/zstd/index.mjs
@@ -82,147 +82,41 @@ const zstdTransformContent = {
 		this.inBufferData.pos.value = 0;
 	},
 
+	/**
+	 * @param {Uint8Array} chunk
+	 */
 	async transform(chunk, controller) {
-		const chunkData = {
-			/** @type {Uint8Array} */
-			chunk: await chunk,
-			pos: 0,
-			get size() {
-				return this.chunk.byteLength;
-			},
-		};
-		/** @type {Uint8Array|null} */
-		let subchunkPadding = null;
+		while (true) {
+			const subchunk = chunk.slice(0, this.inBuffer.size);
+			chunk = chunk.slice(this.inBuffer.size);
 
-		subchunkAssignmentLoop: while (true) {
-			let lastReturnOffset = 0;
-			if (this.lastReturn > 0) {
-				lastReturnOffset = this.lastReturn;
-			}
-			let start = chunkData.pos;
-			let end = Math.min(chunkData.pos + this.inBuffer.size, chunkData.size);
-			let chunk = chunkData.chunk.slice(start, end - lastReturnOffset);
-			if (this.lastReturn > 0) {
-				let newChunk = new Uint8Array(end - start + lastReturnOffset);
-				newChunk.set(subchunkPadding, 0);
-				newChunk.set(chunk, lastReturnOffset);
-				chunk = newChunk;
-				this.lastReturn = 0;
-			}
-			const subchunkData = {
-				chunk,
-				pos: 0,
-				get size() {
-					return this.chunk.byteLength;
-				},
-			};
-
-			this.inBuffer.value = subchunkData.chunk;
+			this.inBuffer.value = subchunk;
 			this.inBufferData.src.value = this.inBuffer.ptr;
-			this.inBufferData.size.value = this.inBuffer.size;
+			this.inBufferData.size.value = subchunk.byteLength;
 			this.inBufferData.pos.value = 0;
 
-			processLoop: while (true) {
-				if (this.lastReturn > 0) {
-					const remainingBufferSize = this.inBuffer.size.value - this.inBuffer.pos.value;
-					if (this.lastReturn < remainingBufferSize) {
-						// TODO:
-						//   - We need to make inBufferData the size of lastReturn.
-						//   - We need to backup the remaining data for the next pass, to put in the next subchunk.
-						const lastReturnBuffer = this.inBuffer.value.slice(
-							this.inBufferData.pos.value,
-							this.inBufferData.pos.value + this.lastReturn,
-						);
-						subchunkPadding = this.inBuffer.value.slice(this.inBufferData.pos.value + this.lastReturn);
-						this.inBuffer.value = lastReturnBuffer;
-						this.inBufferData.src.value = this.inBuffer.ptr;
-						this.inBufferData.size.value = lastReturnBuffer.byteLength;
-						this.inBufferData.pos.value = 0;
-					}
-					this.lastReturn = 0;
+			while (this.inBufferData.pos.value < this.inBufferData.size.value) {
+				this.outBufferData.dst.value = this.outBuffer.ptr;
+				this.outBufferData.size.value = this.outBuffer.size;
+				this.outBufferData.pos.value = 0;
+
+				let ret = wasm._ZSTD_decompressStream(this.ctxPtr, this.outBufferData.ptr, this.inBufferData.ptr);
+				if (wasm._ZSTD_isError(ret)) {
+					controller.error(
+						new Error(
+							`Zstd error:\n[${this.lastReturn}] ${wasm.UTF8ToString(wasm._ZSTD_getErrorName(ret))}`,
+						),
+					);
+					return;
 				}
-
-				flushLoop: while (true) {
-					this.outBufferData.dst.value = this.outBuffer.ptr;
-					this.outBufferData.size.value = this.outBuffer.size;
-					this.outBufferData.pos.value = 0;
-
-					let ret = wasm._ZSTD_decompressStream(this.ctxPtr, this.outBufferData.ptr, this.inBufferData.ptr);
-					if (wasm._ZSTD_isError(ret)) {
-						controller.error(
-							new Error(
-								`Zstd error:\n[${this.lastReturn}] ${wasm.UTF8ToString(wasm._ZSTD_getErrorName(ret))}`,
-							),
-						);
-						return;
-					}
-
-					const uncompressedData = this.outBuffer.value;
-					this.enqueued.push(uncompressedData);
-					controller.enqueue(uncompressedData);
-
-					this.lastReturn = ret;
-
-					if (this.outBufferData.pos.value < this.outBufferData.size.value) {
-						break flushLoop;
-					}
-				}
-
-				if (this.inBufferData.pos.value >= this.inBufferData.size.value) {
-					break processLoop;
-				}
+				controller.enqueue(this.outBuffer.value.slice(0, this.outBufferData.pos.value));
+				this.lastReturn = ret;
 			}
 
-			if (subchunkData.pos >= subchunkData.size) {
-				chunkData.pos = subchunkData.size;
-				break subchunkAssignmentLoop;
+			if (chunk.byteLength === 0) {
+				break;
 			}
 		}
-
-		// let subchunk;
-
-		// chunkAssignmentLoop: while (true) {
-		// 	subchunk = _chunk.slice(this.inBufferData.pos.value, nextInputOffset);
-		// 	if (subchunk.byteLength === 0) {
-		// 		break;
-		// 	}
-
-		// 	subchunkAssignmentLoop: while (true) {
-		// 		this.inBuffer.value = subchunk;
-
-		// 		this.inBufferData.src.value = this.inBuffer.ptr;
-		// 		this.inBufferData.size.value = subchunk.byteLength;
-		// 		this.inBufferData.pos.value = 0;
-
-		// 		subchunkProcessingLoop: while (true) {
-		// 			if (this.inBufferData.pos.value >= this.inBufferData.size.value) {
-		// 				break subchunkProcessingLoop;
-		// 			}
-
-		// 			this.outBufferData.dst.value = this.outBuffer.ptr;
-		// 			this.outBufferData.size.value = this.outBufferSize;
-		// 			this.outBufferData.pos.value = 0;
-
-		// 			let ret = wasm._ZSTD_decompressStream(this.ctxPtr, this.outBufferData.ptr, this.inBufferData.ptr);
-		// 			if (wasm._ZSTD_isError(ret)) {
-		// 				controller.error(
-		// 					new Error(
-		// 						`Zstd error:\n[${this.lastReturn}] ${wasm.UTF8ToString(wasm._ZSTD_getErrorName(ret))}`,
-		// 					),
-		// 				);
-		// 				return;
-		// 			}
-
-		// 			const uncompressedData = this.outBuffer.value;
-		// 			this.enqueued.push(uncompressedData);
-		// 			controller.enqueue(uncompressedData);
-
-		// 			this.lastReturn = ret;
-		// 		}
-
-		// 		// offset = this.inBufferData.size.value;
-		// 	}
-		// }
 	},
 
 	flush(controller) {
@@ -255,7 +149,7 @@ export class ZstdUncompressStream extends TransformStream {
 			inBufferData: null,
 			outBufferData: null,
 			lastReturn: 0,
-			enqueued: [],
+			blob: null,
 		});
 	}
 }
