6e3e76e2f7b20ef89e0c0b7f4490178e19ae8558
Adam Scott <ascott.ca@gmail.com>
2025-04-21 14:55:12 -0400

    Refactor zstd module to use new common utilites

--- platform/web/js/modules/zstd/index.mjs
+++ platform/web/js/modules/zstd/index.mjs
@@ -3,85 +3,11 @@ import { NULLPTR, initWasmUtils } from "@godotengine/common";
 import { default as ZstdWasmModule } from "./zstd.mjs";
 
 export const wasm = await ZstdWasmModule();
-const { mallocOrDie, freeOrDie, sizeOf, WasmValue, WasmStruct } = initWasmUtils(wasm);
+const { WasmValue, WasmStruct, WasmStructMember } = initWasmUtils(wasm);
 
 /**
- * Zstd stream "in-buffer"
- * @typedef {{ srcPtr: number, size: number, pos: number }} ZstdInBuffer
+ * @typedef {import("@godotengine/common").WasmStructMember} WasmStructMember
  */
-
-/**
- * Zstd stream "out-buffer"
- * @typedef {{ dstPtr: number, size: number, pos: number }} ZstdOutBuffer
- */
-
-/**
- * Converts an object to `ZSTD_inBuffer` struct data
- * @param {Object} param
- * @param {number} param.srcPtr
- * @param {number} param.size
- * @param {number} param.pos
- * @returns {Uint8Array}
- */
-function toZSTD_inBuffer({ srcPtr, size, pos }) {
-	const uint32Array = new Uint32Array(3);
-	const dataView = new DataView(uint32Array.buffer);
-	dataView.setUint32(0 * Uint32Array.BYTES_PER_ELEMENT, srcPtr, true);
-	dataView.setUint32(1 * Uint32Array.BYTES_PER_ELEMENT, size, true);
-	dataView.setUint32(2 * Uint32Array.BYTES_PER_ELEMENT, pos, true);
-	return new Uint8Array(uint32Array.buffer);
-}
-
-/**
- * Converts `ZSTD_inBuffer` struct data to an object.
- * @param {TypedArray} zstdInBuffer
- * @returns {ZstdInBuffer}
- */
-function fromZSTD_inBuffer(zstdInBuffer) {
-	if (zstdInBuffer.byteLength !== 3 * Uint32Array.BYTES_PER_ELEMENT) {
-		throw new Error("Invalid `ZSTD_inBuffer` struct data");
-	}
-	const dataView = new DataView(zstdInBuffer.buffer);
-	return {
-		srcPtr: dataView.getUint32(0 * Uint32Array.BYTES_PER_ELEMENT, true),
-		size: dataView.getUint32(1 * Uint32Array.BYTES_PER_ELEMENT, true),
-		pos: dataView.getUint32(2 * Uint32Array.BYTES_PER_ELEMENT, true),
-	};
-}
-
-/**
- * Converts parameters to `ZSTD_outBuffer` struct data
- * @param {Object} param
- * @param {number} param.dstPtr
- * @param {number} param.size
- * @param {number} param.pos
- * @returns {Uint8Array}
- */
-function toZSTD_outBuffer({ dstPtr, size, pos }) {
-	const uint32Array = new Uint32Array(3);
-	const dataView = new DataView(uint32Array.buffer);
-	dataView.setUint32(0 * Uint32Array.BYTES_PER_ELEMENT, dstPtr, true);
-	dataView.setUint32(1 * Uint32Array.BYTES_PER_ELEMENT, size, true);
-	dataView.setUint32(2 * Uint32Array.BYTES_PER_ELEMENT, pos, true);
-	return new Uint8Array(uint32Array.buffer);
-}
-
-/**
- * Converts `ZSTD_outBuffer` struct data to an object.
- * @param {TypedArray} zstdOutBuffer
- * @returns {ZstdOutBuffer}
- */
-function fromZSTD_outBuffer(zstdOutBuffer) {
-	if (zstdOutBuffer.byteLength !== 3 * Uint32Array.BYTES_PER_ELEMENT) {
-		throw new Error("Invalid `ZSTD_outBuffer` struct data");
-	}
-	const dataView = new DataView(zstdOutBuffer.buffer);
-	return {
-		dstPtr: dataView.getUint32(0 * Uint32Array.BYTES_PER_ELEMENT, true),
-		size: dataView.getUint32(1 * Uint32Array.BYTES_PER_ELEMENT, true),
-		pos: dataView.getUint32(2 * Uint32Array.BYTES_PER_ELEMENT, true),
-	};
-}
 
 /**
  * The logic of `ZstdUncompressStream`.
@@ -95,18 +21,59 @@ const zstdTransformContent = {
 
 		this.inBufferSize = wasm._ZSTD_DStreamInSize();
 		this.outBufferSize = wasm._ZSTD_DStreamOutSize();
-		this.inBuffer = new WasmValue(this.inBufferSize);
-		this.outBuffer = new WasmValue(this.outBufferSize);
-		this.inBufferData = new WasmStruct([
-			{ name: "src", type: "*", size: sizeOf("*"), offset: 0 },
-			{ name: "size", type: "u32", size: sizeOf("u32"), offset: sizeOf("*") },
-			{ name: "pos", type: "u32", size: sizeOf("u32"), offset: sizeOf("*") + sizeOf("u32") },
-		]);
-		this.outBufferData = new WasmStruct([
-			{ name: "dst", type: "*", size: sizeOf("*"), offset: 0 },
-			{ name: "size", type: "u32", size: sizeOf("u32"), offset: sizeOf("*") },
-			{ name: "pos", type: "u32", size: sizeOf("u32"), offset: sizeOf("*") + sizeOf("u32") },
-		]);
+		this.inBuffer = new WasmValue({ size: this.inBufferSize });
+		this.outBuffer = new WasmValue({ size: this.outBufferSize });
+
+		/** @type {number!} */
+		let offset;
+		/** @type {WasmStructMember!} */
+		let previousDef;
+
+		// inBuffer struct.
+		offset = 0;
+		/** @type {Record<string, WasmStructMember>} */
+		const inBufferDataDefs = {};
+		previousDef = inBufferDataDefs["src"] = new WasmStructMember({
+			name: "src",
+			type: "void*",
+			offset,
+		});
+		offset += previousDef.size;
+		previousDef = inBufferDataDefs["size"] = new WasmStructMember({
+			name: "size",
+			type: "size_t",
+			offset,
+		});
+		offset += previousDef.size;
+		previousDef = inBufferDataDefs["pos"] = new WasmStructMember({
+			name: "pos",
+			type: "size_t",
+			offset,
+		});
+		this.inBufferData = new WasmStruct(Object.values(inBufferDataDefs));
+
+		// outBuffer struct.
+		offset = 0;
+		/** @type {Record<string, WasmStructMember>} */
+		const outBufferDataDefs = {};
+		previousDef = outBufferDataDefs["dst"] = new WasmStructMember({
+			name: "dst",
+			type: "void*",
+			offset,
+		});
+		offset += previousDef.size;
+		previousDef = outBufferDataDefs["size"] = new WasmStructMember({
+			name: "size",
+			type: "size_t",
+			offset,
+		});
+		offset += previousDef.size;
+		previousDef = outBufferDataDefs["pos"] = new WasmStructMember({
+			name: "pos",
+			type: "size_t",
+			offset,
+		});
+		this.outBufferData = new WasmStruct(Object.values(outBufferDataDefs));
 	},
 
 	async transform(chunk, controller) {
@@ -126,31 +93,35 @@ const zstdTransformContent = {
 			}
 			this.inBuffer.value = subchunk;
 
-			this.inBufferData.set("src", this.inBuffer.ptr);
-			this.inBufferData.set("size", subchunk.byteLength);
-			this.inBufferData.set("pos", 0);
+			this.inBufferData.src.value = this.inBuffer.ptr;
+			this.inBufferData.size.value = subchunk.byteLength;
+			this.inBufferData.pos.value = 0;
 
 			while (true) {
-				if (this.inBufferData.get("pos") >= this.inBuffer.get("size")) {
+				if (this.inBufferData.pos.value >= this.inBuffer.size) {
 					break;
 				}
 
-				this.outBufferData.set("dst", this.outBuffer.ptr);
-				this.outBufferData.set("size", this.outBufferSize);
-				this.outBufferData.set("pos", 0);
+				this.outBufferData.dst.value = this.outBuffer.ptr;
+				this.outBufferData.size.value = this.outBufferSize;
+				this.outBufferData.pos.value = 0;
 
-				let ret;
-				ret = wasm._ZSTD_decompressStream(this.ctxPtr, this.outBufferData.ptr, this.inBufferData.ptr);
-				if (wasm._ZSTD_isError(ret)) {
+				this.lastReturn = wasm._ZSTD_decompressStream(
+					this.ctxPtr,
+					this.outBufferData.ptr,
+					this.inBufferData.ptr,
+				);
+				if (wasm._ZSTD_isError(this.lastReturn)) {
 					controller.error(
-						new Error(`Zstd error:\n[${ret}] ${wasm.UTF8ToString(wasm._ZSTD_getErrorName(ret))}`),
+						new Error(
+							`Zstd error:\n[${this.lastReturn}] ${wasm.UTF8ToString(wasm._ZSTD_getErrorName(this.lastReturn))}`,
+						),
 					);
+					return;
 				}
 
 				const uncompressedData = this.outBuffer.value;
 				controller.enqueue(uncompressedData);
-
-				this.lastReturn = ret;
 			}
 
 			offset += inBuffer.size;
@@ -160,6 +131,8 @@ const zstdTransformContent = {
 	flush(controller) {
 		if (this.lastReturn !== 0) {
 			controller.error(new Error(`Zstd error:\n[${lastRet}]EOF before the end of the stream.`));
+			controller.terminate();
+			return;
 		}
 
 		wasm._ZSTD_freeDCtx(this.ctxPtr);
