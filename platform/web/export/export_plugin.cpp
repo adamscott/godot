48250f4f075b32f83ee8266cc6fe4341d6da0df4
Adam Scott <ascott.ca@gmail.com>
2025-04-21 14:41:47 -0400

    Fix other failing compressions

--- platform/web/export/export_plugin.cpp
+++ platform/web/export/export_plugin.cpp
@@ -166,7 +166,7 @@ Error EditorExportPlatformWeb::_compress_template_files(const Ref<EditorExportPr
 				if (err != OK) {
 					return err;
 				}
-				target->store_buffer(file_bytes.ptr(), compressed_byte_size);
+				target->store_buffer(compressed_bytes.ptr(), compressed_byte_size);
 			}
 		skip_zstd:
 			(void)0;
@@ -189,7 +189,7 @@ Error EditorExportPlatformWeb::_compress_template_files(const Ref<EditorExportPr
 				if (err != OK) {
 					return err;
 				}
-				target->store_buffer(file_bytes.ptr(), compressed_byte_size);
+				target->store_buffer(compressed_bytes.ptr(), compressed_byte_size);
 			}
 		skip_gzip:
 			(void)0;
